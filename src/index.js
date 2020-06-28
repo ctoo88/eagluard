77de1d75700542eefad5c1df8d8a062d1cbc88e9
xu <13958055597@163.com>
2020-06-28 16:48:46 +0800

    map

--- src/index.js
+++ src/index.js
@@ -6,10 +6,12 @@ import imgLoading from './assets/_loading.jpg';
 import imgWorld from './assets/_world.png';
 import jsonYing from './assets/ying.json';
 
+const devicePixelRatio = window.devicePixelRatio || 1;
 const gameConfig = {
   type: Phaser.AUTO,
-  width: window.innerWidth,
-  height: window.innerHeight,
+  width: window.innerWidth * devicePixelRatio,
+  height: window.innerHeight * devicePixelRatio,
+  zoom: 1 / devicePixelRatio,
   physics: {
     default: 'arcade',
     arcade: {
@@ -32,19 +34,18 @@ const gameConfig = {
     }
   }
 };
-
 const game = new Phaser.Game(gameConfig);
 const myFontFamily = '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", Helvetica, Arial, sans-serif';
 let player = {};
-let playerX = 360;
-let playerY = 160;
-let baseSize = 40;
+let playerX = scaleSize(360);
+let playerY = scaleSize(160);
+let baseSize = scaleSize(40);
 
 function preload() {
   const loading = this.add.image(gameConfig.width / 2, gameConfig.height / 2, 'loading');
-  const progressBarFrame = this.add.rectangle((gameConfig.width - 270) / 2, gameConfig.height / 1.2, 270, 15, 0x000000, 0.5);
-  const progressBar = this.add.rectangle((gameConfig.width - 270) / 2, gameConfig.height / 1.2, 100, 15, 0xFFA500);
-  const progressValue = this.add.text(gameConfig.width / 2, gameConfig.height / 1.2);
+  const progressBarFrame = this.add.rectangle((gameConfig.width - scaleSize(270)) / 2, gameConfig.height / 1.2, scaleSize(270), scaleSize(15), 0x000000, 0.5);
+  const progressBar = this.add.rectangle((gameConfig.width - scaleSize(270)) / 2, gameConfig.height / 1.2, 0, scaleSize(15), 0xFFA500);
+  const progressValue = this.add.text(gameConfig.width / 2, gameConfig.height / 1.2, '', { fontSize: scaleSize(16) + 'px' });
   
   loading.setScale(gameConfig.width / loading.width);
   progressBarFrame.setOrigin(0, 0.5);
@@ -52,7 +53,7 @@ function preload() {
   progressValue.setOrigin(0.5, 0.5);
 
   this.load.on('progress', function (value) {
-    progressBar.setSize(270 * value, 15);
+    progressBar.setSize(scaleSize(270 * value), scaleSize(15));
     progressValue.setText(Math.floor(value * 100) + ' %');
   });
 
@@ -63,7 +64,7 @@ function preload() {
     loading.destroy();
   });
 
-  this.load.spritesheet('actor1', spriteActor1, { frameWidth: baseSize, frameHeight: baseSize });
+  this.load.spritesheet('actor1', spriteActor1, { frameWidth: 40, frameHeight: 40 });
   this.load.image('build', imgBuild);
   this.load.image('line', imgLine);
   this.load.image('world', imgWorld);
@@ -78,36 +79,42 @@ function create() {
   const layerGround = mapYing.createStaticLayer('ground', tileWorld);
   const layerBuild = mapYing.createStaticLayer('build', [tileWorld, tileBuild]);
   const layerCover = mapYing.createStaticLayer('cover', tileWorld);
-
+  
   const camera = this.cameras.main;
   const mark = this.add.rectangle(playerX, playerY, baseSize, baseSize, 0x000000, 0.3);
-  const npc = this.physics.add.staticSprite(0, 640, 'actor1', 10);
+  player = this.physics.add.sprite(playerX, playerY, 'actor1', 31);
+  const npc = this.physics.add.staticSprite(0, scaleSize(640), 'actor1', 10);
 
   const dialog = this.add.group();
   const dialogLeft = this.add.arc(baseSize, gameConfig.height - baseSize, baseSize, 90, 270, false, 0x000000, 0.5);
   const dialogMiddle = this.add.rectangle(gameConfig.width / 2, gameConfig.height - baseSize, gameConfig.width - baseSize * 2, baseSize * 2, 0x000000, 0.5);
   const dialogRight = this.add.arc(gameConfig.width - baseSize, gameConfig.height - baseSize, baseSize, 270, 90, false, 0x000000, 0.5);
-  const dialogLineLeft = this.add.image(16, gameConfig.height - baseSize, 'line');
-  const dialogLineRight = this.add.image(gameConfig.width - 16, gameConfig.height - baseSize, 'line');
-  const npcName = this.add.text(baseSize, gameConfig.height - baseSize * 2, '', { fontFamily: myFontFamily });
-  const npcText = this.add.text(gameConfig.width / 2, gameConfig.height - baseSize, '西塞山前白鹭飞， 桃花流水鳜鱼肥', { fontFamily: myFontFamily });
-  const buttonTask = this.add.circle(gameConfig.width - baseSize, gameConfig.height - 252, 20, 0xffffff);
-  const buttonCheck = this.add.circle(gameConfig.width - baseSize, gameConfig.height - 204, 20, 0xffffff);
-  const buttonBattle = this.add.circle(gameConfig.width - baseSize, gameConfig.height - 156, 20, 0xffffff);
-  const buttonCancel = this.add.circle(gameConfig.width - baseSize, gameConfig.height - 108, 20, 0xffffff);
+  const dialogLineLeft = this.add.image(scaleSize(16), gameConfig.height - baseSize, 'line');
+  const dialogLineRight = this.add.image(gameConfig.width - scaleSize(16), gameConfig.height - baseSize, 'line');
+  const npcName = this.add.text(baseSize, gameConfig.height - baseSize * 2, '', { fontFamily: myFontFamily, fontSize: scaleSize(16) + 'px' });
+  const npcText = this.add.text(gameConfig.width / 2, gameConfig.height - baseSize, '西塞山前白鹭飞， 桃花流水鳜鱼肥', { fontFamily: myFontFamily, fontSize: scaleSize(16) + 'px' });
+  const buttonTask = this.add.circle(gameConfig.width - baseSize, gameConfig.height - scaleSize(252), scaleSize(20), 0xff0000);
+  const buttonCheck = this.add.circle(gameConfig.width - baseSize, gameConfig.height - scaleSize(204), scaleSize(20), 0x00ff00);
+  const buttonBattle = this.add.circle(gameConfig.width - baseSize, gameConfig.height - scaleSize(156), scaleSize(20), 0x0000ff);
+  const buttonCancel = this.add.circle(gameConfig.width - baseSize, gameConfig.height - scaleSize(108), scaleSize(20), 0xffffff);
 
   let tagetID = 0;
 
-  player = this.physics.add.sprite(playerX, playerY, 'actor1', 31);
+  layerGround.setScale(devicePixelRatio);
+  layerBuild.setScale(devicePixelRatio);
+  layerCover.setScale(devicePixelRatio);
   layerCover.setDepth(5);
-  camera.startFollow(player, false, 0.4, 0.4);
-  camera.setBounds(0, 0, mapYing.widthInPixels, mapYing.heightInPixels);
-  player.setOrigin(0, 0);
+  camera.startFollow(player, false, scaleSize(0.4), scaleSize(0.4));
+  camera.setBounds(0, 0, scaleSize(mapYing.widthInPixels), scaleSize(mapYing.heightInPixels));
   mark.setOrigin(0, 0);
+  player.setOrigin(0, 0);
+  player.setScale(devicePixelRatio);
   npc.setOrigin(0, 0);
-  npc.setOffset(baseSize / 2, baseSize / 2);
+  npc.setScale(devicePixelRatio);
+  npc.setSize(baseSize, baseSize);
+  npc.setOffset(20, 20);
   npc.setInteractive();
-  npc.tagetID = 20;
+  npc.tagetID = 11;
 
   // dialog group
   dialog.setDepth(10);
@@ -122,6 +129,8 @@ function create() {
   buttonCheck.setDepth(15);
   buttonBattle.setDepth(15);
   buttonCancel.setDepth(15);
+  dialogLineLeft.setScale(devicePixelRatio);
+  dialogLineRight.setScale(devicePixelRatio);
   dialog.add(dialogLeft);
   dialog.add(dialogMiddle);
   dialog.add(dialogRight);
@@ -156,7 +165,7 @@ function create() {
   layerBuild.setCollisionByProperty({ collide: true });
   this.physics.add.collider(player, layerBuild);
   this.physics.add.collider(player, npc, function() {
-    if (tagetID == 20) {
+    if (tagetID == 11) {
       npcName.setText('钓鱼翁');
       dialog.setVisible(true);
       dialog.visible = true;
@@ -201,11 +210,11 @@ function create() {
 
       if (moveX()) {
         player.anims.play(moveX() > 0 ? 'right' : 'left', true);
-        player.body.setVelocityX(moveX() > 0 ? 80 : -80);
+        player.body.setVelocityX(moveX() > 0 ? scaleSize(80) : scaleSize(-80));
       }
 
       if (moveY()) {
-        player.body.setVelocityY(moveY() > 0 ? 80 : -80);
+        player.body.setVelocityY(moveY() > 0 ? scaleSize(80) : scaleSize(-80));
       }
     }
   }, this);
@@ -216,14 +225,14 @@ function create() {
 
 function update() {
   if (moveX()) {
-    if (Math.abs(moveX()) < 2) {
+    if (Math.abs(moveX()) < scaleSize(2)) {
       player.body.setVelocityX(0);
       player.setPosition(playerX, player.y);
     }
   }
-
+  
   if (moveY()) {
-    if (Math.abs(moveY()) < 2) {
+    if (Math.abs(moveY()) < scaleSize(2)) {
       player.body.setVelocityY(0);
       player.setPosition(player.x, playerY);
     }
@@ -231,6 +240,9 @@ function update() {
 }
 
 // utils
+function scaleSize(num) {
+  return num * devicePixelRatio;
+}
 function formatPoint(num) {
   return Math.floor(num / baseSize) * baseSize;
 }
